b569dcd71c9aa24373f4085fdc5d83a5830f0fc2
Elucidator_xrb <rubixue572@gmail.com>
2022-04-05 10:27:41 +0800

    Better debug in main.c not init.c, so change back

--- init/init.c
+++ init/init.c
@@ -10,11 +10,6 @@ void mips_init()
 {
 	printf("init.c:\tmips_init() is called\n");
 	
-	/*----- printf test -start -------*/
-//	printf("test %%:{ %\n;%\\n  }\n");
-//	printf("test %%s:{ %s;\%s }\n", "ab", "cd");
-	/*----- printf test -end  ------- */
-
 	//for your degree,don't delete these.
 	//------------|
 	#ifdef FTEST
