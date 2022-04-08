fedf821a46eda7db7df8bf59505704d0f93e7f5b
Elucidator_xrb <rubixue572@gmail.com>
2022-04-08 12:16:19 +0800

    Finish till Exercise 2.5 - physical memory management

--- mm/pmap.c
+++ mm/pmap.c
@@ -27,8 +27,11 @@ void mips_detect_memory()
 {
 	/* Step 1: Initialize basemem.
 	 * (When use real computer, CMOS tells us how many kilobytes there are). */
-
+	basemem	= 0x04000000;	// 64MB = 2^26B -- 2^26 = (2^4)^6 * 2^2 = 0x0400_0000
+	maxpa	= basemem;
+	extmem	= 0;
 	// Step 2: Calculate corresponding npage value.
+	npage	= basemem >> PGSHIFT;	// 64MB/4KB = 16K -- 64M >> 2^12
 
 	printf("Physical memory: %dK available, ", (int)(maxpa / 1024));
 	printf("base = %dK, extended = %dK\n", (int)(basemem / 1024),
@@ -174,18 +177,28 @@ Hint:
 Use `LIST_INSERT_HEAD` to insert something to list.*/
 void page_init(void)
 {
+	extern char end[]; 
 	/* Step 1: Initialize page_free_list. */
 	/* Hint: Use macro `LIST_INIT` defined in include/queue.h. */
-
+	LIST_INIT(&page_free_list);
 
 	/* Step 2: Align `freemem` up to multiple of BY2PG. */
+	ROUND(freemem, BY2PG);
 
-
-	/* Step 3: Mark all memory blow `freemem` as used(set `pp_ref`
+	/* Step 3: Mark all memory below `freemem` as used(set `pp_ref`
 	 * filed to 1) */
-
-
+	struct Page* cur;
+	for (cur = pages; page2kva(cur) < freemem; ++cur) {
+		cur->pp_ref = 1;
+	}
+//printf("[trace]: PADDR(x):%lx, maxpa:%lx, page2kva(cur):%lx\n", 
+//						PADDR(freemem), maxpa, page2kva(cur));
 	/* Step 4: Mark the other memory as free. */
+	while (page2ppn(cur) < npage) { // page2ppn is more direct than page2pa
+		cur->pp_ref = 0;
+		LIST_INSERT_HEAD(&page_free_list, cur, pp_link);
+		++ cur;
+	}
 }
 
 /* Exercise 2.4 */
@@ -208,12 +221,15 @@ int page_alloc(struct Page **pp)
 	struct Page *ppage_temp;
 
 	/* Step 1: Get a page from free memory. If fail, return the error code.*/
-
+	if (LIST_EMPTY(&page_free_list)) return -E_NO_MEM;
+	ppage_temp = LIST_FIRST(&page_free_list);
+	LIST_REMOVE(ppage_temp, pp_link);
 
 	/* Step 2: Initialize this page.
 	 * Hint: use `bzero`. */
-
-
+	bzero(page2kva(ppage_temp), BY2PG);
+	*pp = ppage_temp;
+	return 0;
 }
 
 /* Exercise 2.5 */
@@ -224,10 +240,13 @@ When you free a page, just insert it to the page_free_list.*/
 void page_free(struct Page *pp)
 {
 	/* Step 1: If there's still virtual address referring to this page, do nothing. */
-
+	if (pp->pp_ref > 0) return;
 
 	/* Step 2: If the `pp_ref` reaches 0, mark this page as free and return. */
-
+	if (pp->pp_ref == 0) {
+		LIST_INSERT_HEAD(&page_free_list, pp, pp_link);
+		return;
+	}
 
 	/* If the value of `pp_ref` is less than 0, some error must occurr before,
 	 * so PANIC !!! */
@@ -449,9 +468,12 @@ void physical_memory_manage_check(void)
 	//LIST_FIRST(&test_free) = &test_pages[0];
 	int i,j=0;
 	struct Page *p, *q;
+//printf("Here\n");
+
 	//test inert tail
 	for(i=0;i<10;i++) {
 		test_pages[i].pp_ref=i;
+//printf("&test_free:%lx, &test_pages[%d]:%lx\n", &test_free, i, &test_pages[i]);
 		//test_pages[i].pp_link=NULL;
 		//printf("0x%x  0x%x\n",&test_pages[i], test_pages[i].pp_link.le_next);
 		LIST_INSERT_TAIL(&test_free,&test_pages[i],pp_link);
