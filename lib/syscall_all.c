e948842d3bb52cbfceb0269e62ec7d8b3435a5eb
Elucidator_xrb <rubixue572@gmail.com>
2022-05-19 03:13:29 +0800

    Change envid2env()'s parameter: checkperm from 1 to 0 except sys_mem_alloc()

--- lib/syscall_all.c
+++ lib/syscall_all.c
@@ -119,7 +119,7 @@ int sys_set_pgfault_handler(int sysno, u_int envid, u_int func, u_int xstacktop)
 	struct Env *env;
 	int ret;
 
-    if ((ret = envid2env(envid, &env, 1))) return ret;
+    if ((ret = envid2env(envid, &env, 0))) return ret;
     env->env_pgfault_handler = func;
     env->env_xstacktop = xstacktop;
 
@@ -194,8 +194,8 @@ int sys_mem_map(int sysno, u_int srcid, u_int srcva, u_int dstid, u_int dstva,
 
     if (srcva >= UTOP || dstva >= UTOP) return -E_INVAL;
     if ((perm & PTE_V) == 0)            return -E_INVAL;
-    if ((ret = envid2env(srcid, &srcenv, 1))) return ret;
-    if ((ret = envid2env(dstid, &dstenv, 1))) return ret;
+    if ((ret = envid2env(srcid, &srcenv, 0))) return ret;
+    if ((ret = envid2env(dstid, &dstenv, 0))) return ret;
 
     ppage = page_lookup(srcenv->env_pgdir, round_srcva, &ppte);
     if (ppage == NULL) return -E_INVAL;
@@ -224,7 +224,7 @@ int sys_mem_unmap(int sysno, u_int envid, u_int va)
 	struct Env *env;
 
     if (va >= UTOP) return -E_INVAL;
-    if ((ret = envid2env(envid, &env, 1))) return ret;
+    if ((ret = envid2env(envid, &env, 0))) return ret;
     page_remove(env->env_pgdir, va);
 
 	return ret;
@@ -281,7 +281,7 @@ int sys_set_env_status(int sysno, u_int envid, u_int status)
 
     if (status != ENV_RUNNABLE && status != ENV_NOT_RUNNABLE && status != ENV_FREE)
         return -E_INVAL;
-    if ((ret = envid2env(envid, &env, 1))) return ret;
+    if ((ret = envid2env(envid, &env, 0))) return ret;
 
     if (status == ENV_FREE) env_destroy(env);
     else {
